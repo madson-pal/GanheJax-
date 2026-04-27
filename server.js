d39108375ea4290be893622b4d5ac0798b7e4530
madson-pal <madsonsantos123456789@gmail.com>
2026-04-27 10:32:19 -0300

    Add '/tarefas' route to serve external iframe

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+
 app.get('/', (req, res) => {
   res.send(`
     <html>
@@ -42,3 +45,14 @@ app.get('/', (req, res) => {
     </html>
   `);
 });
+
+app.get('/tarefas', (req, res) => {
+  const url = "https://offers.cpx-research.com/index.php?app_id=32644&ext_user_id=1";
+
+  res.send(`
+    <h1 style="text-align:center;">Tarefas disponíveis</h1>
+    <iframe src="${url}" width="100%" height="600"></iframe>
+  `);
+});
+
+app.listen(3000, () => console.log("Rodando"));
